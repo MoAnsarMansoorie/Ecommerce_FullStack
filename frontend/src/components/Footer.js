94601c3219456613d9259e98777389a6a41742e1
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2024-01-03 22:16:45 +0530

    router setup with page

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light p-3'>
+      <h2 className='text-center'>
+        All right reserverd &copy; ansar
+      </h2>
+    </div>
+  )
+}
+
+export default Footer
