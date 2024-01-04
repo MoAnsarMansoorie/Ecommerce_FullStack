42ff4891f43e3f70814ecf79eb969591f58d030a
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2024-01-04 23:17:35 +0530

    context auth setup

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { FaShoppingBag } from "react-icons/fa";
+import { useAuth } from "../contexts/auth";
+import toast from "react-hot-toast";
 
 const Header = () => {
+  const [auth, setAuth] = useAuth();
+
+  const handleLogout = () => {
+    setAuth({
+      ...auth,
+      user: null,
+      token: "",
+    });
+    localStorage.removeItem("auth")
+    toast.success("LogOut successfully!")
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -33,16 +47,32 @@ const Header = () => {
                   Category
                 </NavLink>
               </li>
-              <li className="nav-item">
-                <NavLink to="/register" className="nav-link">
-                  Register
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/login" className="nav-link">
-                  LogIn
-                </NavLink>
-              </li>
+              {!auth.user ? (
+                <>
+                  <li className="nav-item">
+                    <NavLink to="/register" className="nav-link">
+                      Register
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink to="/login" className="nav-link">
+                      LogIn
+                    </NavLink>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <NavLink
+                      to="/login"
+                      onClick={handleLogout}
+                      className="nav-link"
+                    >
+                      LogOut
+                    </NavLink>
+                  </li>
+                </>
+              )}
               <li className="nav-item">
                 <NavLink to="/cart" className="nav-link">
                   Cart {0}
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { useAuth } from '../contexts/auth'
 
 function HomePage() {
+  const [auth, setAuth] = useAuth()
+  console.log('====================================');
+  console.log(auth);
+  console.log('====================================');
   return (
     <Layout >
         <h1>Homepage</h1>
+        <pre>{JSON.stringify(auth, null, 4)}</pre>
     </Layout>
   )
 }
