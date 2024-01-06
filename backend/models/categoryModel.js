13d0f0876dfb1b80edbef4e8c410b745d10f4e31
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2024-01-07 01:59:16 +0530

    category api controller done

---
+++ backend/models/categoryModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+
+const categotySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        slug: {
+            type: String,
+            lowercase: true
+        }
+    }
+)
+
+export default mongoose.model("Category", categotySchema)
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from "cors"
 import colors from "colors"
 import connectToDb from "./config/db.js"
 import authRoute from "./routes/authRoute.js"
+import categoryRoute from "./routes/categoryRoute.js"
 
 // configure dotenv
 dotenv.config()
@@ -22,6 +23,7 @@ app.use(morgan("dev"))
 
 // routes
 app.use("/api/v1/auth", authRoute)
+app.use("/api/v1/category", categoryRoute)
 
 const PORT = process.env.PORT || 8080
 
